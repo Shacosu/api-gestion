a0b700aabc9e73dac2ca995daac8781d4db1ca41
Shacosu <sh4c0p@gmail.com>
2023-09-09 20:16:28 -0300

    little fix

--- src/controllers/scrapController.js
+++ src/controllers/scrapController.js
@@ -37,84 +37,84 @@ module.exports = {
 
     await Promise.all([
 
-      // cluster.queue(async ({ page }) => {
-      //   console.log('Trabajando con juegosdigitaleschile.com'.red);
-      //   const gamesList = await Link.find({ "link": { $regex: "juegosdigitaleschile.com" } });
-      //   const validateList = await Games.find();
-      //   for (const game of gamesList) {
-      //     const gameLink = game.link;
-      //     console.log("Link:", gameLink)
-      //     const category = game.category;
-      //     let validate = validateList.some(x => x.url === gameLink)
-      //     if (!validate) {
-      //       try {
-      //         const response = await axios.get(gameLink);
-      //         const $ = cheerio.load(response.data);
-      //         const data = {
-      //           title: $('.underlined.pb-10.mb-20').text().trim(),
-      //           sku: new Date(),
-      //           price: $('#final_price > div.price-new').text().replace(/\,|\$|\CLP|/g, '').trim(),
-      //           image: $('body > div:nth-child(11) > div:nth-child(1) > div:nth-child(1) > div.col-sm-12.col-xs-12.col-md-70.pb-20 > div > div > div.col-xs-12.col-sm-5 > div > img').attr('src'),
-      //           description: $('#description').text().trim(),
-      //           discount: $('#descuento-tag').text() || "0%",
-      //           provider: new URL(gameLink).hostname || "Sin proveedor",
-      //           url: gameLink,
-      //           category,
-      //         }
-      //         let gameInfo = new Games(data)
-      //         await gameInfo.save();
-      //       } catch (error) {
-      //         console.log(error.message)
-      //       }
-      //     } else {
-      //       const response = await axios.get(gameLink);
-      //       const $ = cheerio.load(response.data);
-      //       const price = $('#final_price > div.price-new').text().replace(/\,|\$|\CLP|/g, '').trim();
-      //       try {
-      //         await Games.findOneAndUpdate({ url: gameLink }, { price: Number(price) });
-      //       } catch (error) {
-      //         console.log(error)
-      //       }
-      //     }
-      //   }
+      cluster.queue(async ({ page }) => {
+        console.log('Trabajando con juegosdigitaleschile.com'.red);
+        const gamesList = await Link.find({ "link": { $regex: "juegosdigitaleschile.com" } });
+        const validateList = await Games.find();
+        for (const game of gamesList) {
+          const gameLink = game.link;
+          console.log("Link:", gameLink)
+          const category = game.category;
+          let validate = validateList.some(x => x.url === gameLink)
+          if (!validate) {
+            try {
+              const response = await axios.get(gameLink);
+              const $ = cheerio.load(response.data);
+              const data = {
+                title: $('.underlined.pb-10.mb-20').text().trim(),
+                sku: new Date(),
+                price: $('#final_price > div.price-new').text().replace(/\,|\$|\CLP|/g, '').trim(),
+                image: $('body > div:nth-child(11) > div:nth-child(1) > div:nth-child(1) > div.col-sm-12.col-xs-12.col-md-70.pb-20 > div > div > div.col-xs-12.col-sm-5 > div > img').attr('src'),
+                description: $('#description').text().trim(),
+                discount: $('#descuento-tag').text() || "0%",
+                provider: new URL(gameLink).hostname || "Sin proveedor",
+                url: gameLink,
+                category,
+              }
+              let gameInfo = new Games(data)
+              await gameInfo.save();
+            } catch (error) {
+              console.log(error.message)
+            }
+          } else {
+            const response = await axios.get(gameLink);
+            const $ = cheerio.load(response.data);
+            const price = $('#final_price > div.price-new').text().replace(/\,|\$|\CLP|/g, '').trim();
+            try {
+              await Games.findOneAndUpdate({ url: gameLink }, { price: Number(price) });
+            } catch (error) {
+              console.log(error)
+            }
+          }
+        }
 
-      // }, { page: 1, priority: 1, taskId: 1 }),
+      }, { page: 1, priority: 1, taskId: 1 }),
 
 
-      // cluster.queue(async ({ page }) => {
-      //   console.log('Trabajando con chilejuegosdigitales.com'.blue);
-      //   const gamesList = await Link.find({ "link": { $regex: "chilejuegosdigitales.cl" } });
-      //   const validateList = await Games.find({ "provider": { $regex: "chilejuegosdigitales.cl" } });
-      //   for (const game of gamesList) {
-      //     const { link, category } = game;
-      //     console.log("Link:", link)
-      //     const response = await axios.get(link);
-      //     const $ = cheerio.load(response.data);
-      //     const gameInfo = {
-      //       title: $("#ProductSection > div.grid > div.grid-item.large--three-fifths > h1").text().trim(),
-      //       price: Number($('#productPrice-product-template > span.visually-hidden').text().replace(/\,|\$|\CLP|\./g, '').trim()),
-      //       image: `https:${$(".product-photo-container > img").attr('src')}`,
-      //       description: $('.product-description').text().trim(),
-      //       provider: new URL(link).hostname || "Sin proveedor",
-      //       discount: $("#descuento-tag").text() || "0%",
-      //       url: link,
-      //       category
-      //     }
-      //     const validate = validateList.some(x => x.url === link);
-      //     if (validate) {
-      //       try {
-      //         const price = $('#productPrice-product-template > span.visually-hidden').text().replace(/\,|\$|\CLP|\./g, '').trim();
-      //         const updateGame = await Games.findOneAndUpdate({ url: link }, { price: Number(price) });
-      //       } catch (error) {
-      //         console.log(error)
-      //       }
-      //     } else {
-      //       const savedGame = new Games(gameInfo);
-      //       await savedGame.save();
-      //     }
+      cluster.queue(async ({ page }) => {
+        console.log('Trabajando con chilejuegosdigitales.com'.blue);
+        const gamesList = await Link.find({ "link": { $regex: "chilejuegosdigitales.cl" } });
+        const validateList = await Games.find({ "provider": { $regex: "chilejuegosdigitales.cl" } });
+        for (const game of gamesList) {
+          const { link, category } = game;
+          console.log("Link:", link)
+          const response = await axios.get(link);
+          const $ = cheerio.load(response.data);
+          const gameInfo = {
+            title: $("#ProductSection > div.grid > div.grid-item.large--three-fifths > h1").text().trim(),
+            price: Number($('#productPrice-product-template > span.visually-hidden').text().replace(/\,|\$|\CLP|\./g, '').trim()),
+            image: `https:${$(".product-photo-container > img").attr('src')}`,
+            description: $('.product-description').text().trim(),
+            provider: new URL(link).hostname || "Sin proveedor",
+            discount: $("#descuento-tag").text() || "0%",
+            url: link,
+            category
+          }
+          const validate = validateList.some(x => x.url === link);
+          if (validate) {
+            try {
+              const price = $('#productPrice-product-template > span.visually-hidden').text().replace(/\,|\$|\CLP|\./g, '').trim();
+              const updateGame = await Games.findOneAndUpdate({ url: link }, { price: Number(price) });
+            } catch (error) {
+              console.log(error)
+            }
+          } else {
+            const savedGame = new Games(gameInfo);
+            await savedGame.save();
+          }
 
-      //   }
-      // }, { page: 2, priority: 2, taskId: 2 }),
+        }
+      }, { page: 2, priority: 2, taskId: 2 }),
 
       cluster.queue(async ({ page }) => {
         console.log('Trabajando con eneba.com'.yellow);
