194b32709fda0448ed63eefd599dae39ce19338d
Shacosu <sh4c0p@gmail.com>
2023-09-09 20:41:30 -0300

    fix

--- src/controllers/scrapController.js
+++ src/controllers/scrapController.js
@@ -128,7 +128,7 @@ module.exports = {
           const { link: game, category } = gameItem;
           try {
             console.log("Link:", game)
-            await page.goto(game, { waitUntil: 'DOMContentLoaded' });
+            await page.goto(game, { waitUntil: 'domcontentloaded' });
             const html = await page.content();
             const $ = cheerio.load(html);
             const price = $('meta[property="product:price:amount"]').attr('content');
